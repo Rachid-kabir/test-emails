c20186fa6a154c81a199838977f42574e8435af9
Rachid-kabir <52111231+Rachid-kabir@users.noreply.github.com>
2023-09-07 12:52:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
     experimental: {
         serverActions: true,
     },
+    api: {
+        bodyParser: {
+          sizeLimit: '1mb',
+        },
+        externalResolver: true,
+        timeout: 30000, // Set the timeout to 30 seconds (in milliseconds)
+    },
 }
 
 module.exports = nextConfig
